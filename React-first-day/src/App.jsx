e54db1c58cf8ce74e33cdab048cab2459b547d71
KyungJoon960 <kjkim77960@gmail.com>
2022-07-05 08:38:21 +0000

    React first day

---
+++ React-first-day/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Box from "./Box";
+
+function App() {
+  return (
+    <main>
+      <Box name = " : Kim" num = "1"/>
+      <Box name = " : Kyung" num = "2"/>
+      <Box name = " : Joon" num = "3"/>
+      <Box name = " : KJ" num = "4"/>
+    </main>
+  );
+}
+
+export default App;
